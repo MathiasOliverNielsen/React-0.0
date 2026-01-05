26e47297983adf08f45f8658820074b8a9693262
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2026-01-05 11:22:13 +0100

    ...

---
+++ React-0.0/src/components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: "#282c34",
+        padding: "20px",
+        textAlign: "center",
+        color: "white",
+        marginBottom: "20px",
+      }}
+    >
+      <h1>Positives and Negatives about React</h1>
+    </header>
+  );
+}
+
+export default Header;
